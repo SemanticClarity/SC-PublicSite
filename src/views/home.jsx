d59f7824af98553aaaf4243390a77496c94193d7
karencalpo <minnkc@gmail.com>
2021-07-26 17:50:49 -0700

    Created import variable for image

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -7,6 +7,7 @@ import Partners from "./partners.jsx";
 import About from "./about.jsx";
 import News from "./news.jsx";
 import Footer from "./footer.jsx";
+import Clouds from "../images/dreamstime.jpg";
 
 class Home extends Elements.Component {
   constructor(props = {}) {
@@ -18,7 +19,7 @@ class Home extends Elements.Component {
       <div id={this.props.id} name={this.props.name} className="home">
         <div className="hero-text">
           <Hero />
-          <img src="../images/dreamstime.jpg" />
+          <img src={Clouds} />
         </div>
         {/* <ButtonBar className="section">
           <Button name="contact" id="contact" icon="chat" title="Contact Us" kind="secondary">&nbsp;Contact Us Today</Button>
